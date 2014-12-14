50b22d573dc3788ae71a97208472911f3277be57
celie56 <celie56@gmail.com>
2014-12-13 20:33:18 -0500

    added skeleton library and basic functionality

---
+++ js/chris.js
@@ -0,0 +1,12 @@
+
+// happens at start
+$(function(){
+	$(".shide").toggle();
+	$(".shide:first").toggle();
+});
+
+$(".choice").click(function(){
+	$loc = "." + $(this).attr("id");
+	$(".shide").hide();
+	$($loc).toggle();
+});
