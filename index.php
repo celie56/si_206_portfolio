1ec4752d1c58f6ca9380d6cfc184803cfcf9a3cc
celie56 <celie56@gmail.com>
2014-12-13 22:06:27 -0500

    Big changes, much feature

--- index.php
+++ index.php
@@ -4,8 +4,54 @@
 		<title>Elie Home</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<?php include "style.html"; ?>
+		<?php include "scripts.html"; ?>
 	</head>
 	<body>
-		<h1>Chris Elie</h1>
+		<div class="container">
+			<div>
+				<h1>Chris Elie</h1>
+			</div>
+			<div class="row">
+				<div class="two columns">
+					<h2><a id="about" href="#" class="choice">About</a></h2>
+				</div>
+				<div class="ten columns">
+					<div class="about shide">
+						<?php include "pages/about.html"; ?>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="two columns">
+					<h2><a id="resume" class="choice" href="#">Resume</a></h2>
+				</div>
+				<div class="ten columns">
+					<div class="resume shide">
+						<?php include "pages/resume.html"; ?>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="two columns">
+					<h2><a id="contact" class="choice" href="#">Contact</a></h2>
+				</div>
+				<div class="ten columns">
+					<div class="contact shide">
+						<?php include "pages/contact.html"; ?>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="two columns">
+					<h2><a id="photos" class="choice" href="#">Photos</a></h2>
+				</div>
+				<div class="ten columns">
+					<div class="photos shide">
+						<?php include "pages/photos.html"; ?>
+					</div>
+				</div>
+			</div>
+		</div>
 	</body>
 </html>
--- js/chris.js
+++ js/chris.js
@@ -3,10 +3,13 @@
 $(function(){
 	$(".shide").toggle();
 	$(".shide:first").toggle();
+
+	$(".choice").click(function(){
+		$loc = "." + $(this).attr("id");
+		$(".shide").hide();
+		$($loc).toggle();
+	});
+
+	$(".yoxview").yoxview();
 });
 
-$(".choice").click(function(){
-	$loc = "." + $(this).attr("id");
-	$(".shide").hide();
-	$($loc).toggle();
-});
